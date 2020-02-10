10e6d85110bd324e30ed0a55e53d2b90304ad442
ethylene <enelyhte@gmail.com>
2020-02-10 16:24:16 +0800

    fix some bug.

--- PS288.ino
+++ PS288.ino
@@ -5,8 +5,10 @@
 #define DATAPIN 4           //ps2 data pin
 #define IRQPIN  3           //ps2 clock pin
 PS2KeyAdvanced keyboard;
-uint16_t c;
-unsigned int buff=0;
+uint16_t c,buff=0;
+char a=0,p=0,brkflag=0;
+
+
 const unsigned int K[120]={0xFFC0, 0xFFA0, 0xFF60, 0xFEE0, 0xFDE0, 0xFBE0, 0xF7E0, 0xEFE0,
                            0xDFC2, 0xDFA2, 0xDF62, 0xDEE2, 0xDDE2, 0xDBE2, 0xD7E2, 0xCFE2,
                            0xDFC4, 0xDFA4, 0xDF64, 0xDEE4, 0xDDE4, 0xDBE4, 0xD7E4, 0xCFE4,
@@ -22,6 +24,8 @@ const unsigned int K[120]={0xFFC0, 0xFFA0, 0xFF60, 0xFEE0, 0xFDE0, 0xFBE0, 0xF7E
                            0xFFD8, 0xFFB8, 0xFF78, 0xFEF8, 0xFDF8, 0xFBF8, 0xF7F8, 0xEFF8,
                            0xDFDA, 0xDFBA, 0xDF7A, 0xDEFA, 0xDDFA, 0xDBFA, 0xD7FA, 0xCFFA,
                            0xDFDC, 0xDFBC, 0xDF7C, 0xDEFC, 0xDDFC, 0xDBFC, 0xD7FC, 0xCFFC};   //pc88 code array
+
+
                            
 const unsigned int KO[120]={0x30,  0x31,  0x32,  0x33,  0x34,  0x35,  0x36,  0x37,
                             0x38,  0x39,  0x2E,  0x2C,  0x5F,  0x3B,  0x3D,  0x11E,
@@ -39,61 +43,65 @@ const unsigned int KO[120]={0x30,  0x31,  0x32,  0x33,  0x34,  0x35,  0x36,  0x3
                             0x30C, 0x50B, 0x112, 0x40,  0x30,  0x30,  0x30,  0x30,
                             0x11E, 0x2B,  0x4106,  0x4107,  0x30,  0x30,  0x30,  0x30};       //ps2 code array
 
+
+
 const unsigned int BRK[15]={0xDFE0, 0xFFE2 ,0xFFE4 ,0xDFE6 ,0xFFE8 ,0xDFEA ,0xDFEC ,0xFFEE ,
                             0xFFF0, 0xDFF2, 0xDFF4, 0xFFF6, 0xDFF8, 0xFFFA, 0xFFFC};                 //break code array
-char a=0,p=0,q=0;
-bool brkflag=0;
-void SENDKEY(int bitvar)       //key sending func
+
+
+
+void SENDKEY(uint16_t data,char dl)       //key sending func
 {
   a=0;
-  buff=bitvar;  //load keycode
+  buff=data;  //load keycode
   TCCR1A = 0;  // set Timer register to 0
   TCCR1B = 0;  
   OCR1A = 763;                          // 768 in theory,acutually 763 is the best
   TCCR1B |= (1 << WGM12);               // turn on CTC mode: 
   TIMSK1 |= (1 << OCIE1A);              // enable timer compare interrupt:
-  TCCR1B |= (1 << CS10);                // Set  mode  
-  while(a<15)                           //wait the sending finish
+  TCCR1B |= (1 << CS10);                // Set  mode 
+  while(a<16)                           //wait the sending finish
   {
-    digitalWrite(LED_BUILTIN, HIGH);    //light up the LED when sending
-    digitalWrite(LED_BUILTIN, LOW);
- 
+    digitalWrite(LED_BUILTIN, HIGH);
   }
   TCCR1B &= ~(1 << CS10);    //stop the timer clock
   TIMSK1 &= ~(1 << OCIE1A);  //stop the timer interrupt
-  delay(36);                 //slightly high input latency when fast typing,
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(dl);                 //slightly high input latency when fast typing,
                              //but codes with the same row number must be sent after 35ms , dont know why
 }
 
 ISR(TIMER1_COMPA_vect) //ISR func
 { 
-  
-    digitalWrite(TXPIN,bitRead(buff,a));//send 1bit
-    a++;
-  
+  digitalWrite(TXPIN,bitRead(buff,a));//send 1bit
+  a++;
 }
 
+void clean()
+{
+  SENDKEY(BRK[0],3);
+  SENDKEY(BRK[1],3);
+  SENDKEY(BRK[2],3);
+  SENDKEY(BRK[3],3);
+  SENDKEY(BRK[4],3);
+  SENDKEY(BRK[5],3);
+  SENDKEY(BRK[6],3);
+  SENDKEY(BRK[7],3);
+  SENDKEY(BRK[8],3);
+  SENDKEY(BRK[9],3);
+  SENDKEY(BRK[10],3);
+  SENDKEY(BRK[11],3);
+  SENDKEY(BRK[12],3);
+  SENDKEY(BRK[13],3);
+  SENDKEY(BRK[14],3);
+}
 
 
 void setup() {
   keyboard.begin( DATAPIN, IRQPIN );  //PS2Keyboard service
   pinMode(TXPIN,OUTPUT);              //Tx pin init
   digitalWrite(TXPIN, HIGH);
-  SENDKEY(BRK[0]);                    //reset fix freezing
-  SENDKEY(BRK[1]);
-  SENDKEY(BRK[2]);
-  SENDKEY(BRK[3]);
-  SENDKEY(BRK[4]);
-  SENDKEY(BRK[5]);
-  SENDKEY(BRK[6]);
-  SENDKEY(BRK[7]);
-  SENDKEY(BRK[8]);
-  SENDKEY(BRK[9]);
-  SENDKEY(BRK[10]);
-  SENDKEY(BRK[11]);
-  SENDKEY(BRK[12]);
-  SENDKEY(BRK[13]);
-  SENDKEY(BRK[14]);
+  clean();                    //reset fix freezing
 }
 
 
@@ -112,38 +120,38 @@ void loop() {
       {
         if(p==102)                        //extend keys INS
         {
-          SENDKEY(K[102]);
-          SENDKEY(K[70]);
-          SENDKEY(K[67]);
+          SENDKEY(K[102],36);
+          SENDKEY(K[70],36);
+          SENDKEY(K[67],36);
         }
         else if (p<101&&p>95)             //extend keys F6-F10
         {
-          SENDKEY(K[p]);
-          SENDKEY(K[70]);
-          SENDKEY(K[p-23]);
+          SENDKEY(K[p],36);
+          SENDKEY(K[70],36);
+          SENDKEY(K[p-23],36);
         }
         else if (p==101||p==103)          //extend keys BS DEL
         {
-          SENDKEY(K[p]);
-          SENDKEY(K[67]);
+          SENDKEY(K[p],36);
+          SENDKEY(K[67],36);
         }
         else if (p==104||p==105)          //extend keys HENKAN KETTEI
         {
-          SENDKEY(K[p]);
-          SENDKEY(K[78]);
+          SENDKEY(K[p],36);
+          SENDKEY(K[78],36);
         }
         else if (p==112||p==113)          //extend keys RETURN RETURN(NUMPAD)
         {
-          SENDKEY(K[p]);
-          SENDKEY(K[15]);
+          SENDKEY(K[p],36);
+          SENDKEY(K[15],36);
         }
         else if (p==114||p==115)          //extend keys LSHIFT RSHIFT
         {
-          SENDKEY(K[p]);
-          SENDKEY(K[70]);
+          SENDKEY(K[p],36);
+          SENDKEY(K[70],36);
         }
         else 
-        SENDKEY(K[p]);                    //normal keys
+        SENDKEY(K[p],36);     //normal keys
         break;
         }
      }
@@ -153,49 +161,44 @@ void loop() {
      {
       if ((c&0x00ff)==(KO[p]&0x00ff))     //same as make
       {
+        
         if(p==102)
         {
-          SENDKEY(BRK[12]);
-          SENDKEY(BRK[8]);
-          SENDKEY(BRK[8]);
+          SENDKEY(BRK[12],36);
+          SENDKEY(BRK[8],36);
+          SENDKEY(BRK[8],36);
         }
         else if (p<101&&p>95)
         {
-          SENDKEY(BRK[12]);
-          SENDKEY(BRK[9]);
-          SENDKEY(BRK[8]);
+          SENDKEY(BRK[12],36);
+          SENDKEY(BRK[9],36);
+          SENDKEY(BRK[8],36);
         } 
         else if (p==101||p==103)
         {
-          SENDKEY(BRK[12]);
-          SENDKEY(BRK[8]);
+          SENDKEY(BRK[12],36);
+          SENDKEY(BRK[8],36);
         }
         else if (p==104||p==105)
         {
-          SENDKEY(BRK[13]);
-          SENDKEY(BRK[9]);
+          SENDKEY(BRK[13],36);
+          SENDKEY(BRK[9],36);
         }
         else if (p==112||p==113)
         {
-          SENDKEY(BRK[14]);
-          SENDKEY(BRK[1]);
+          SENDKEY(BRK[14],36);
+          SENDKEY(BRK[1],36);
         }
         else if (p==114||p==115)
         {
-          SENDKEY(BRK[14]);
-          SENDKEY(BRK[8]);
+          SENDKEY(BRK[14],36);
+          SENDKEY(BRK[8],36);
         }
         else
-        SENDKEY(BRK[p/8]);
+        SENDKEY(BRK[p/8],36);
         break;
         }
-     }
+      }
     }
   }
-
-  
 }
-
-
-
-
